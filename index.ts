93bbe3f1272df14d83c27699dc970eebe3d9fa03
widcardw <lui747353367@qq.com>
2022-11-13 23:29:38 +0800

    chore: ignore private information

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from './utils/define-config'
 import Shota from './plugin/shota'
 import Tap from './plugin/tap'
+import { groups } from './private/groups'
 
 defineConfig({
   plugins: [
     Shota({
       validGroupUsers: [],
-      validGroups: [498993303],
+      validGroups: Object.values(groups),
     }),
     Tap,
   ],
