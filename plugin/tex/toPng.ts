399fa08c7580617c5c2962d55c0188af5905c92a
widcardw <lui747353367@qq.com>
2023-02-16 21:43:47 +0800

    feat: add padding to tex image (inspired by Fran)

--- plugin/tex/toPng.ts
+++ plugin/tex/toPng.ts
@@ -3,8 +3,19 @@ import os from 'os'
 import path from 'path'
 import { Resvg } from '@resvg/resvg-js'
 
+function addPadding(svg: string): string {
+  return svg.replace(/viewBox="([^"]*)"/, (_match, $1: string) => {
+    const viewBox = $1.split(' ')
+      .map(parseFloat)
+      .map((x, i) => i < 2 ? x - 100 : x + 200)
+      .join(' ')
+    return `viewBox="${viewBox}"`
+  })
+}
+
 async function toPng(svg: string) {
   svg = svg.match(/<svg(.*)<\/svg>/)![0]
+  svg = addPadding(svg)
   const resvg = new Resvg(svg, {
     background: 'rgb(255,255,255)',
     fitTo: {
